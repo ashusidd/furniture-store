e3eb0bfcfef5f705616a2e0db7ad9ed2032d0b06
Ashu Sidd <www.sonuali0011@gmail.com>
2026-04-26 13:14:16 +0530

    PWA icons and config added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Ashraf Woods Furniture',
+        short_name: 'AshrafWoods',
+        description: 'Premium Furniture Store by Ashraf Ali',
+        theme_color: '#ea580c', // Orange theme
+        background_color: '#FDFBF7', // Website background color
+        display: 'standalone', // Isse app jaisi feeling aayegi (no browser URL bar)
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
